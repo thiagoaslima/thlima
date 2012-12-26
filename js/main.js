57028a1b317ce4847dba7cde83113f96db5c86e4
Thiago <thiagoaslima@gmail.com>
2012-12-26 19:29:10 -0200

    losangos posicionados em 1440px

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+(function ($) {
+    "use strict";
 
+    if ($("html").hasClass("svg")) {
+        var imgs = $(".i_svg"),
+            i = imgs.length,
+            src,
+            src2;
+
+        while (i) {
+            i -= 1;
+            src = $(imgs[i]).attr("src");
+            src2 = src.replace("png", "svg");
+
+            $(imgs[i]).attr("src", src2);
+        }
+    }
+
+}(jQuery));
